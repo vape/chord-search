32e662a5b44f2b6d654e66eeedf6d867eb410d9f
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-17 09:42:20 +0200

    Implemented retrieval of song data
    Implemented parsing of song page with BeautifulSoup4
    to retrieve song title, artist name, chords and
    song rating (if exists).

--- indexer.py
+++ indexer.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 
 ns_clean_re = compile(r'xmlns=\".*\"')
 chord_url_re = compile(r'.*_crd\.htm$')
+rating_re = compile(r'x\s*(\d+)')
+song_title_clean_re = compile(r' Chords$')
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
 
@@ -20,13 +22,21 @@ def get_chord_urls(sitemap_url):
 
 
 def get_song_data(song_url):
+    soup = BeautifulSoup(get(song_url, headers=headers).text)
+    chords = list(set([c.text for c in soup.select('#cont span')]))
 
+    title = song_title_clean_re.sub('', soup.select('.t_title h1')[0].text)
+    artist = soup.select('.t_autor a')[0].text
+    rt_match = rating_re.match(soup.select('.raiting .v_c')[0].text.strip())
+    rating = int(rt_match.group(1)) if rt_match else None
+    print(title, artist, rating)
 
 
 def main():
-    sitemaps = get_sitemaps()
-    urls = get_chord_urls(sitemaps[-1])
-    [print(u) for u in urls]
+    #sitemaps = get_sitemaps()
+    #urls = get_chord_urls(sitemaps[-1])
+    #[print(u) for u in urls]
+    print(get_song_data('http://tabs.ultimate-guitar.com/o/oasis/a_bell_will_ring_crd.htm'))
 
 
 if __name__ == '__main__':
