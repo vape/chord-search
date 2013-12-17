4678a2548b823210dc9981b1815ede2e8ae0ea49
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-17 09:11:36 +0200

    Implemented sitemap.xml parsing

--- indexer.py
+++ indexer.py
@@ -1,2 +1,19 @@
-from orm import dbsession, Chord, Song
+from xml.etree import ElementTree as ET
+from requests import get
+from re import sub, search
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
+
+
+def get_sitemaps():
+    root = ET.fromstring(sub(r"xmlns=\".*\"", "", get('http://www.ultimate-guitar.com/sitemap.xml', headers=headers).text))
+    return [l.text for l in root.findall('sitemap/loc') if search(r'sitemap\d+\.xml$', l.text)]
+
+
+def main():
+    sitemaps = get_sitemaps()
+    print(sitemaps)
+
+
+if __name__ == '__main__':
+    main()
