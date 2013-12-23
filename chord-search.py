2ecf8dad4b4c0da1168d2337a522119f38e1fd34
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-23 09:27:02 +0200

    Improved search parameter handling, rearranged layout

--- chord-search.py
+++ chord-search.py
@@ -4,14 +4,25 @@ from orm import dbsession, Chord
 app = Flask(__name__)
 
 
+def _get_selected_chords():
+    return list(map(int, request.args.getlist('crd')))
+
+
+def _get_all_chords():
+    return dbsession.query(Chord).order_by(Chord.name).all()
+
+
 @app.route('/')
 def index():
-    chords = dbsession.query(Chord).order_by(Chord.name).all()
-    return render_template('index.html', chords=chords)
+    return render_template('index.html', chords=_get_all_chords(), sel_chords=_get_selected_chords())
+
 
 @app.route('/search', methods=['GET'])
 def search():
-    return render_template('index.html')
+    chord_ids = list(map(int, request.args.getlist('crd')))
+    query = request.args.get('q')
+    print(query, chord_ids)
+    return render_template('index.html', chords=_get_all_chords(), sel_chords=_get_selected_chords())
 
 
 if __name__ == '__main__':
