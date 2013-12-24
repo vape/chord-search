62d633c1386e24d865915cdb07a6003f67595136
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-24 12:16:31 +0200

    Completed implementation of paging control

--- chord-search.py
+++ chord-search.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request
-from orm import dbsession, Chord, Song
-from sqlalchemy import not_
 from datetime import datetime
 
+from flask import Flask, render_template, request
+from lib.pagination import Pagination
+from orm import dbsession, Chord, Song
+from sqlalchemy import not_
+from lib.template_helpers import url_for_other_page
+
+
 app = Flask(__name__)
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
 PAGE_SIZE = 10
+
 
 def _get_selected_chords():
     return list(map(int, request.args.getlist('crd')))
@@ -14,9 +20,16 @@ def _get_all_chords():
     return dbsession.query(Chord).order_by(Chord.name).all()
 
 
-def _search(q, chords, page=0):
+def _get_current_page():
+    try:
+        return int(request.args.get('p', '1'))
+    except ValueError:
+        return 1
+
+
+def _search(q, chords, page=1):
     if not q and not chords:
-        return [], 0
+        return [], 0, 0
     st = datetime.now()
     q1 = dbsession.query(Song).join(Song.chords).filter(Chord.id.in_(chords))
     if q:
@@ -24,7 +37,7 @@ def _search(q, chords, page=0):
     q2 = dbsession.query(Song).join(Song.chords).filter(not_(Chord.id.in_(chords))) if len(chords) > 1 else None
     q = q1.except_(q2) if q2 else q1
     cnt = q.count()
-    res = q.limit(PAGE_SIZE).offset(page * PAGE_SIZE).all()
+    res = q.limit(PAGE_SIZE).offset((page-1) * PAGE_SIZE).all()
     end = datetime.now()
     return res, cnt, (end - st).total_seconds()
 
@@ -36,8 +49,17 @@ def index():
 
 @app.route('/search', methods=['GET'])
 def search():
-    results, total_count, elapsed = _search(request.args.get('q'), list(map(int, request.args.getlist('crd'))), int(request.args.get('p', '0')))
-    return render_template('search_results.html', results=results, total_count=total_count, elapsed=elapsed, chords=_get_all_chords(), sel_chords=_get_selected_chords())
+    results, total_count, elapsed = _search(request.args.get('q'), list(map(int, request.args.getlist('crd'))),
+                                            _get_current_page())
+    page_data = {
+        'results': results,
+        'total_count': total_count,
+        'elapsed': elapsed,
+        'chords': _get_all_chords(),
+        'sel_chords': _get_selected_chords(),
+        'pagination': Pagination(_get_current_page(), PAGE_SIZE, total_count)
+    }
+    return render_template('search_results.html', **page_data)
 
 
 if __name__ == '__main__':
