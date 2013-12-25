19d7baee82ed94161da543aaf72b457d4d6be344
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-25 14:22:59 +0200

    Added environment variable check to config.py
    If the required keys are all in os.environ, then
    config.py does not bother with env.yaml and returns
    immediately.
    
    This change was made so that the app could work on
    Heroku (or other similar environments). Since the
    env.yaml file will not be deployed, the configuration
    settings must be obtained from environment variables
    in the prod environment.

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import yaml
 from os import path, environ
 
+def contains(small, big):
+    for i in range(1 + len(big) - len(small)):
+        if small == big[i:i+len(small)]:
+            return i, i + len(small) - 1
+    return False
+
 def initialize_config(config_file_name='env.yaml'):
+    config_keys = ['DBSERVER', 'DBNAME', 'DBUSER', 'DBPASS', 'DBPORT']
+    if contains(config_keys, environ.keys()):
+        return
+
     config_file_path = path.join(path.dirname(path.abspath(__file__)), config_file_name)
 
     if not path.exists(config_file_path):
