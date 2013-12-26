a04c1dd8ab1b894f2f1fff460c8a44c65811a764
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-26 10:03:20 +0200

    Removed print statement from pagination helper

--- lib/template_helpers.py
+++ lib/template_helpers.py
@@ -2,6 +2,5 @@ from flask import request, url_for
 
 def url_for_other_page(page):
     args = request.args.copy()
-    print(args)
     args['p'] = page
     return url_for(request.endpoint, **args)
